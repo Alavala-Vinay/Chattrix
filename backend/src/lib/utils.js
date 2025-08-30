d1e49522cfadace0c370887b8c56a30b1ed97f93
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-08-30 22:48:22 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "None",  // ✅ fixes "Unauthorized - No Token Provided" on Vercel
-    SameSite="Secure",
+    sameSite: "None", secure: true,
     
     secure: process.env.NODE_ENV !== "development", // cookie only over HTTPS in prod
   });
