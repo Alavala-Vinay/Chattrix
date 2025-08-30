e1f6901b81767dec750ebeeaf1cc95514f503f7d
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-08-30 21:56:07 +0530

    Update axios.js

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chattrix-steel.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chattrix-y8fo.onrender.com/api",
   withCredentials: true,
 });
